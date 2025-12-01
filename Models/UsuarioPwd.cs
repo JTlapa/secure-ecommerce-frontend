de48d22f6e5c34e029ac82a76108634171399e7d
Zaid <zaidoskate@hotmail.com>
2025-12-01 01:36:59 -0600

    validaciones en CU crear cuenta zaid

--- Models/UsuarioPwd.cs
+++ Models/UsuarioPwd.cs
@@ -11,11 +11,15 @@ public class UsuarioPwd
 
     [Required(ErrorMessage = "El campo {0} es obligatorio.")]
     [MinLength(6, ErrorMessage = "El campo {0} debe tener un mínimo de {1} caracteres.")]
+    [RegularExpression(@"^.*(?=.*[\W_]).*$", ErrorMessage = "La contraseña debe contener al menos un caracter especial (ej. @, #, $, etc).")]
     [DataType(DataType.Password)]
     [Display(Name = "Contraseña")]
     public required string Password { get; set; }
 
     [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+    [StringLength(50, MinimumLength = 1, ErrorMessage = "El nombre debe tener entre 1 y 50 caracteres.")]
+    [RegularExpression(@"^[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ\s]+$", ErrorMessage = "El nombre solo puede contener letras y espacios.")]
+    [Display(Name = "Nombre completo")]
     public required string Nombre { get; set; }
 
     [Required(ErrorMessage = "El campo {0} es obligatorio.")]
